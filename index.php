9dbab393888daa774914e3dcedada2c931bd2b37
modul10 <alvithops@gmail.com>
2026-02-07 17:49:16 +0700

    update contrib 14

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <!DOCTYPE HTML>
 <html>
-     <head>
-    <title>OOP PHP</title>
-    </head>
-    <body>
-        <?php
-        if(isset($_GET['r'])){
-            $r = $_GET['r'];
-            if($r == "persegi"){
-                require_once 'persegi/formpersegi.php';
-            }
-        } else {
-            ?>
-            <a href='index.php?r=persegi'>Persegi Panjang</a>
-            <?php
-        }
-    </body>
+<head>
+<title>OOP PHP</title>
+</head>
+<body>
+<?php
+if(isset($_GET['r'])){
+$r = $_GET['r'];
+if($r == "persegi"){
+require_once 'persegi/formpersegi.php';
+}
+}else{
+?>
+<a href='index.php?r=persegi'>Persegi Panjang</a>
+<?php } ?>
+</body>
 </html>
