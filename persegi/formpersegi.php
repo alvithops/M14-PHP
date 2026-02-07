c2af0aaa644b676fb1ea14d673156fddbdef937c
modul10 <alvithops@gmail.com>
2026-02-07 17:58:50 +0700

    update contrib 27

--- persegi/formpersegi.php
+++ persegi/formpersegi.php
@@ -1,6 +1,8 @@
 <h3>Persegi Panjang</h3>
 <form action='persegi/prosespersegi.php' method='post'>
     <table>
-        <tr></tr>
+        <tr>
+            <th></th>
+        </tr>
     </table>
 </form>
