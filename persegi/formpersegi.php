868b76fa5b3193f2680c7359e9a62e01805b1f32
modul10 <alvithops@gmail.com>
2026-02-07 18:09:12 +0700

    update contrib 44

--- persegi/formpersegi.php
+++ persegi/formpersegi.php
@@ -1,18 +1,18 @@
 <h3>Persegi Panjang</h3>
 <form action='persegi/prosespersegi.php' method='post'>
-    <table>
-        <tr>
-            <th>Panjang</th>
-            <td><input type='text' name='panjang'/></td>
-        </tr>
-        <tr>
-            <th>Lebar</th>
-            <td><input type='text' name='lebar'/></td>
-        </tr>
-        <tr>
-            <td colspan='2' align='center'>
-                <input type='submit' value='submit'/></td>td>
-            </td>
-        </tr>
-    </table>
+<table>
+<tr>
+<th>Panjang</th>
+<td><input type='text' name='panjang'/></td>
+</tr>
+<tr>
+5
+<th>Lebar</th>
+<td><input type='text' name='lebar'/></td>
+</tr>
+<tr>
+<td colspan='2' align='center'>
+<input type='submit' value='Submit'/></td>
+</tr>
+</table>
 </form>
