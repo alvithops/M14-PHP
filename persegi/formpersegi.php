82bc1450ed0c8c09e6e7107650609a9260dfd76b
modul10 <alvithops@gmail.com>
2026-02-07 18:03:06 +0700

    update contrib 34

--- persegi/formpersegi.php
+++ persegi/formpersegi.php
@@ -5,5 +5,12 @@
             <th>Panjang</th>
             <td><input type='text' name='panjang'/></td>
         </tr>
+        <tr>
+            <th>Lebar</th>
+            <td><input type='text' name='lebar'/></td>
+        </tr>
+        <tr>
+            <td colspan='2'><input type='submit' value='Hitung'/></td>
+        </tr>
     </table>
 </form>
