4622e4287cbecd5a3ffe003c1880788532f2c662
modul10 <alvithops@gmail.com>
2026-02-07 21:36:13 +0700

    update contrib 162

--- persegi/tampilpersegi.php
+++ persegi/tampilpersegi.php
@@ -7,7 +7,7 @@
         <h3>Persegi Panjang</h3>
         <table>
             <tr>
-                <th>Panjang s :</th>
+                <th>Panjang se :</th>
                 <td><?php echo $pp->panjang; ?></td>
             </tr>
             <tr>
