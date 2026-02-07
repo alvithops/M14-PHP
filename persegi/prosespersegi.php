d1c0a3d4a25a9aabeeb1e93fd1847b4562bd3831
modul10 <alvithops@gmail.com>
2026-02-07 18:30:08 +0700

    update contrib 86

--- persegi/prosespersegi.php
+++ persegi/prosespersegi.php
@@ -2,13 +2,11 @@
 require_once 'PersegiPanjang.php';
 $panjang = $_POST['panjang'];
 $lebar = $_POST['lebar'];
-
 //ciptakan objek dr kelas PersegiPanjang
 $pp = new PersegiPanjang();
 //isi data form ke properti
 $pp->panjang = $panjang;
 $pp->lebar = $lebar;
-
 //tampilkan hasil
 require_once 'tampilpersegi.php';
 ?>
