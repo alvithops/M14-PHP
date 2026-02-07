3409cf25c3fb78b839c7cf757ba27f42ab032503
modul10 <alvithops@gmail.com>
2026-02-07 17:43:45 +0700

    update contrib 3

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         if(isset($_GET['r'])){
             $r = $_GET['r'];
             if($r == "persegi"){
-                require_once
+                require_once 'persegi'
             }
         }
     </body>
