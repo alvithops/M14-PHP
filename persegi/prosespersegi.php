86bdcd0b3a71caca06b141465d9770a0d69abe59
modul10 <alvithops@gmail.com>
2026-02-07 18:17:25 +0700

    update contrib 56

--- persegi/prosespersegi.php
+++ persegi/prosespersegi.php
@@ -1,3 +1,11 @@
 <?php
 require_once 'PersegiPanjang.php';
 $panjang = $_POST['panjang'];
+$lebar = $_POST['lebar'];
+
+$persegiPanjang = new PersegiPanjang($panjang, $lebar);
+
+echo "Panjang: " . $panjang . "<br>";
+echo "Lebar: " . $lebar . "<br>";
+echo "Luas: " . $persegiPanjang->hitungLuas() . "<br>";
+echo "Keliling: " . $persegiPanjang->hitungKeliling() . "<br>";
